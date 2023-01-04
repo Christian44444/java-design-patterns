60aeb60820ee1ee477a52b0538dcc44503dfe400
Christian44444 <c.michaud@laposte.net>
2023-01-04 15:18:48 +0100

    2ième version

--- src/fr/diginamic/factory/testFactory.java
+++ src/fr/diginamic/factory/testFactory.java
@@ -8,8 +8,8 @@ public class testFactory {
 		ObjetConnecte t1 = O1.getObjetConnecte(Type.Telephone, 10);
 		ObjetConnecte t2 = O1.getObjetConnecte(Type.Tablette, 12);
 		ObjetConnecte t3 = O1.getObjetConnecte(Type.Enceinte, 20);
-		System.out.println(t1.getClass());
-		System.out.println(t2.getClass());
-		System.out.println(t3.getClass());
+		System.out.println(t1.getClass() + " " + t1.getLimiteVolts());
+		System.out.println(t2.getClass() + " " + t2.getLimiteVolts());
+		System.out.println(t3.getClass() + " " + t3.getLimiteVolts());
 	}
 }
